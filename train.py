24ae6397b4e43709ca14a41c5f57b4f56330f6c7
Basil L. Contovounesios <contovob@tcd.ie>
2020-11-15 01:51:22 +0000

    PEP-8-ify train.py a bit
    Lines longer than 80 chars still remain.

--- train.py
+++ train.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
 
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
 import os
 import cv2
 import numpy
-import string
 import random
 import argparse
 import tensorflow as tf
 import tensorflow.keras as keras
 
+
 # Build a Keras model given some parameters
 def create_model(captcha_length, captcha_num_symbols, input_shape, model_depth=5, module_size=2):
-  input_tensor = keras.Input(input_shape)
-  x = input_tensor
-  for i, module_length in enumerate([module_size] * model_depth):
-      for j in range(module_length):
-          x = keras.layers.Conv2D(32*2**min(i, 3), kernel_size=3, padding='same', kernel_initializer='he_uniform')(x)
-          x = keras.layers.BatchNormalization()(x)
-          x = keras.layers.Activation('relu')(x)
-      x = keras.layers.MaxPooling2D(2)(x)
+    input_tensor = keras.Input(input_shape)
+    x = input_tensor
+    for i, module_length in enumerate([module_size] * model_depth):
+        for j in range(module_length):
+            x = keras.layers.Conv2D(32*2**min(i, 3), kernel_size=3, padding='same', kernel_initializer='he_uniform')(x)
+            x = keras.layers.BatchNormalization()(x)
+            x = keras.layers.Activation('relu')(x)
+        x = keras.layers.MaxPooling2D(2)(x)
 
-  x = keras.layers.Flatten()(x)
-  x = [keras.layers.Dense(captcha_num_symbols, activation='softmax', name='char_%d'%(i+1))(x) for i in range(captcha_length)]
-  model = keras.Model(inputs=input_tensor, outputs=x)
+    x = keras.layers.Flatten()(x)
+    x = [keras.layers.Dense(captcha_num_symbols, activation='softmax', name='char_%d'%(i+1))(x) for i in range(captcha_length)]
+    model = keras.Model(inputs=input_tensor, outputs=x)
 
-  return model
+    return model
+
 
 # A Sequence represents a dataset for training in Keras
 # In this case, we have a folder full of images
@@ -81,6 +78,7 @@ class ImageSequence(keras.utils.Sequence):
                 y[j][i, self.captcha_symbols.find(ch)] = 1
 
         return X, y
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -174,6 +172,7 @@ def main():
         except KeyboardInterrupt:
             print('KeyboardInterrupt caught, saving current weights as ' + args.output_model_name+'_resume.h5')
             model.save_weights(args.output_model_name+'_resume.h5')
+
 
 if __name__ == '__main__':
     main()
