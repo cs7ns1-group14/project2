d1fad9c6fad50d80acffb1add8b7cd82da0086af
Basil L. Contovounesios <contovob@tcd.ie>
2020-11-16 05:14:03 +0000

    Make run.sh more verbose and deactivate

--- run.sh
+++ run.sh
@@ -35,12 +35,25 @@ done
 [ -z "${model:-}" -o -z "${directory:-}" -o \
      -z "${output:-}" -o -z "${syms:-}" ] && usage
 
+echo "Model    : ${model}"
+echo "CAPTCHAs : ${directory}"
+echo "Symbols  : ${syms}"
+echo "Output   : ${output}"
+
 if [ ! -d .venv ]; then
+  echo 'No .venv directory found; creating'
   python3 -m venv .venv
+  echo 'Activating .venv'
   . .venv/bin/activate
+  echo 'Installing requirements in .venv'
   pip3 install -r requirements.txt
 elif [ ! -x "$(command -v deactivate)" ]; then
+  echo 'Activating .venv'
   . .venv/bin/activate
 fi
 
+echo 'Running classification'
 ./classifylite.py -m "${model}" -d "${directory}" -o "${output}" -S "${syms}"
+
+echo 'Deactivating .venv'
+deactivate
